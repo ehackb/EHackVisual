ccf0a4d00f57167ce46fbda7dce9e20c8501e99e
swasse <davidvansteertegem@david-van-steertegems-MacBook.local>
2015-11-27 09:51:24 +0100

    Finished generators
    -can generate notes from socket
    -moves notes
    -notes give feedback on their position
    -notes destroyed when invisible

---
+++ Assets/scripts/GeneratePrefabs.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using SocketIO;
+
+public class GeneratePrefabs : MonoBehaviour {
+
+	public Transform first_note;
+
+	private SocketIOComponent socket;
+
+	void Start () {
+		GameObject go = GameObject.Find("SocketIO");
+		socket = go.GetComponent<SocketIOComponent>();
+
+		socket.On("message", TestMessage);
+	}
+
+	void Update () {
+
+	}
+
+	public void TestMessage(SocketIOEvent e)
+	{
+		Debug.Log(string.Format("[name: {0}, data: {1}]", e.name, e.data));
+
+		float x = Random.Range (-4f, 2f);
+		float y = Random.Range (-3f, 3f);
+
+		Instantiate(first_note, new Vector3(x, y, 0), Quaternion.identity);
+	}
+
+
+}
---
+++ Assets/scripts/update_note.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using SocketIO;
+
+public class update_note : MonoBehaviour {
+
+	public float timeoutDestructor;
+
+	private Rigidbody body;
+	private SocketIOComponent socket;
+
+	void Start () {
+		timeoutDestructor = 200;
+
+		GameObject go = GameObject.Find("SocketIO");
+		socket = go.GetComponent<SocketIOComponent>();
+
+		body = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate () {
+		float x = transform.position.x + Random.Range(0.01f ,0.05f);
+		float y = transform.position.y + Random.Range(-0.02f ,0.02f);
+		float z = transform.position.z;
+
+		Vector3 moveTo = new Vector3 (x, y, z);
+
+		body.MovePosition(moveTo);
+
+		JSONObject jsonObj = new JSONObject();
+		jsonObj.AddField ("scale", x);
+		jsonObj.AddField ("pitch", y);
+		jsonObj.AddField ("name", this.name);
+		socket.Emit ("note_move", jsonObj);
+	}
+}
